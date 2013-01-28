423e43f6e8b4d01015ba9b9bf3326ee1fdcc76f9
cassio <cassio@csc.(none)>
2013-01-28 19:19:52 -0300

    colocando publish stream

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '156023987880730','1fbca9e2cb99ac52d8d3e65fabd84ed4', {:scope => 'email, offline_access', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}} 
+  provider :facebook, '156023987880730','1fbca9e2cb99ac52d8d3e65fabd84ed4', {:scope => 'publish_stream, email, offline_access', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}} 
 end
