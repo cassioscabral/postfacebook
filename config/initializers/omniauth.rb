101efad4308b2f5873e4d52cc783bd1ec16acde1
cassio <cassio@csc.(none)>
2013-01-28 18:40:24 -0300

    asdjsad

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, 'OMNIAUTH_PROVIDER_KEY','OMNIAUTH_PROVIDER_SECRET', {:scope => 'email, offline_access', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}} 
+  provider :facebook, '156023987880730','1fbca9e2cb99ac52d8d3e65fabd84ed4', {:scope => 'email, offline_access', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}} 
 end
