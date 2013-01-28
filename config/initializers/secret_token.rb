e43a603e5d186fd31b71733ced31a7774de82ce4
cassio <cassio@csc.(none)>
2013-01-28 17:36:01 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Postfacebook::Application.config.secret_token = '1696cc9c22b5edee800d8f79d138194bd570cad588609482ba70b3d6f59347822c20d9cc82545138cf970527487e76284ead3e67141934ba0f651cdd8ada09c0'
